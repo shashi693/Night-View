3e748f44e91463ffeee9c42b9f8f02abec5eae3d
shashi693 <sukenkar@hotmail.com>
2017-02-13 07:10:16 +0530

    not view
    no view

---
+++ app/src/main/java/com/avenueinfotech/location/MainActivity.java
@@ -0,0 +1,369 @@
+package com.avenueinfotech.location;
+
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback,GoogleApiClient.ConnectionCallbacks,  GoogleApiClient.OnConnectionFailedListener, LocationListener {
+
+    private GoogleMap mMap = null;
+    GoogleApiClient mGoogleApiClient;
+
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    private static final String SELECTED_STYLE = "selected_style";
+
+    private int mSelectedStyleId = R.string.style_label_default;
+
+    private int mStyleIds[] = {
+            R.string.style_label_retro,
+            R.string.style_label_night,
+            R.string.style_label_grayscale,
+            R.string.style_label_no_pois_no_transit,
+            R.string.style_label_default,
+    };
+
+    private static final LatLng SYDNEY = new LatLng(-33.8688, 151.2091);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (savedInstanceState != null) {
+            mSelectedStyleId = savedInstanceState.getInt(SELECTED_STYLE);
+        }
+//        setContentView(R.layout.activity_main);
+
+
+
+        if (googleServicesAvailable()) {
+            Toast.makeText(this, "GPS Enabled", Toast.LENGTH_SHORT).show();
+            setContentView(R.layout.activity_main);
+            initMap();
+        } else {
+            Toast.makeText(this, "Enable GPS", Toast.LENGTH_LONG).show();
+        }
+
+
+
+    }
+
+    private void initMap() {
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapFragment);
+        mapFragment.getMapAsync(this);
+    }
+
+    public boolean googleServicesAvailable() {
+        GoogleApiAvailability api = GoogleApiAvailability.getInstance();
+        int isAvailable = api.isGooglePlayServicesAvailable(this);
+        if (isAvailable == ConnectionResult.SUCCESS) {
+            return true;
+        } else if (api.isUserResolvableError(isAvailable)) {
+            Dialog dialog = api.getErrorDialog(this, isAvailable, 0);
+            dialog.show();
+        } else {
+            Toast.makeText(this, "Cant connect to play services", Toast.LENGTH_LONG).show();
+        }
+        return false;
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        // Store the selected map style, so we can assign it when the activity resumes.
+        outState.putInt(SELECTED_STYLE, mSelectedStyleId);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap map) {
+        mMap = map;
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(SYDNEY, 14));
+        setSelectedStyle();
+
+        if (mMap != null) {
+
+            mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
+                @Override
+                public void onMarkerDragStart(Marker marker) {
+
+                }
+
+                @Override
+                public void onMarkerDrag(Marker marker) {
+
+                }
+
+                @Override
+                public void onMarkerDragEnd(Marker marker) {
+                    Geocoder gc = new Geocoder(MainActivity.this);
+                    LatLng ll = marker.getPosition();
+                    double lat = ll.latitude;
+                    double lng = ll.longitude;
+                    List<Address> list = null;
+                    try {
+                        list = gc.getFromLocation(lat, lng, 1);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                    Address add = list.get(0);
+                    marker.setTitle(add.getAddressLine(1));
+                    marker.showInfoWindow();
+
+
+                }
+            });
+
+            mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                @Override
+                public View getInfoWindow(Marker marker) {
+                    return null;
+                }
+
+                @Override
+                public View getInfoContents(Marker marker) {
+                    View v = getLayoutInflater().inflate(R.layout.info_window, null);
+
+                    TextView tvLocality = (TextView) v.findViewById(R.id.tv_locality);
+                    TextView tvLat = (TextView) v.findViewById(R.id.tv_lat);
+                    TextView tvLng = (TextView) v.findViewById(R.id.tv_lng);
+//                    TextView tvSnippet = (TextView) v.findViewById(R.id.tv_snippet);
+
+
+                    LatLng ll = marker.getPosition();
+                    tvLocality.setText(marker.getTitle());
+                    tvLat.setText("Latiude: " + ll.latitude);
+                    tvLng.setText("Longitude: " + ll.longitude);
+//                    tvSnippet.setText(marker.getSnippet());
+
+
+                    return v;
+                }
+            });
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+        map.setMyLocationEnabled(true);
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(LocationServices.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
+
+        mGoogleApiClient.connect();
+    }
+
+    private void goToLocationZoom(double lat, double lng, float zoom) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 16);
+        mMap.moveCamera(update);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.styled_map, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_style_choose) {
+            showStylesDialog();
+        }
+        return true;
+    }
+
+    private void showStylesDialog() {
+        List<String> styleNames = new ArrayList<>();
+        for (int style : mStyleIds) {
+            styleNames.add(getString(style));
+        }
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(getString(R.string.style_choose));
+        builder.setItems(styleNames.toArray(new CharSequence[styleNames.size()]),
+                new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        mSelectedStyleId = mStyleIds[which];
+                        String msg = getString(R.string.style_set_to, getString(mSelectedStyleId));
+                        Toast.makeText(getBaseContext(), msg, Toast.LENGTH_SHORT).show();
+                        Log.d(TAG, msg);
+                        setSelectedStyle();
+                    }
+                });
+        builder.show();
+    }
+
+    private void setSelectedStyle() {
+        MapStyleOptions style;
+        switch (mSelectedStyleId) {
+            case R.string.style_label_retro:
+                // Sets the retro style via raw resource JSON.
+                style = MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_retro);
+                break;
+            case R.string.style_label_night:
+                // Sets the night style via raw resource JSON.
+                style = MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_night);
+                break;
+            case R.string.style_label_grayscale:
+                // Sets the grayscale style via raw resource JSON.
+                style = MapStyleOptions.loadRawResourceStyle(this, R.raw.mapstyle_grayscale);
+                break;
+            case R.string.style_label_no_pois_no_transit:
+                // Sets the no POIs or transit style via JSON string.
+                style = new MapStyleOptions("[" +
+                        "  {" +
+                        "    \"featureType\":\"poi.business\"," +
+                        "    \"elementType\":\"all\"," +
+                        "    \"stylers\":[" +
+                        "      {" +
+                        "        \"visibility\":\"off\"" +
+                        "      }" +
+                        "    ]" +
+                        "  }," +
+                        "  {" +
+                        "    \"featureType\":\"transit\"," +
+                        "    \"elementType\":\"all\"," +
+                        "    \"stylers\":[" +
+                        "      {" +
+                        "        \"visibility\":\"off\"" +
+                        "      }" +
+                        "    ]" +
+                        "  }" +
+                        "]");
+                break;
+            case R.string.style_label_default:
+                // Removes previously set style, by setting it to null.
+                style = null;
+                break;
+            default:
+                return;
+        }
+        mMap.setMapStyle(style);
+    }
+
+    LocationRequest mLocationRequest;
+
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+        mLocationRequest = LocationRequest.create();
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setInterval(300000);
+        {
+            if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+        }
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+        if(location == null){
+            Toast.makeText(this, "Cant get current location", Toast.LENGTH_LONG).show();
+        } else {
+            LatLng ll = new LatLng(location.getLatitude(), location.getLongitude());
+            CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, 19);
+            mMap.animateCamera(update);
+        }
+    }
+
+    Marker marker;
+
+    public void geoLocate(View view) throws IOException {
+
+        EditText et  = (EditText) findViewById(R.id.editText);
+        String location = et.getText().toString();
+
+        Geocoder gc = new Geocoder(this);
+        List<Address> list = gc.getFromLocationName(location, 1);
+        Address address = list.get(0);
+        String locality = address.getLocality();
+//        String area = address.getLocality();
+
+
+        Toast.makeText(this, locality, Toast.LENGTH_LONG).show();
+
+
+        double lat = address.getLatitude();
+        double lng = address.getLongitude();
+        goToLocationZoom(lat, lng, 25);
+
+        setMarker(locality, lat, lng);
+
+    }
+
+    private void setMarker(String locality, double lat, double lng) {
+        if (marker != null) {
+            marker.remove();
+        }
+
+        MarkerOptions options = new MarkerOptions()
+                .title(locality)
+                .draggable(true)
+                .icon(BitmapDescriptorFactory.fromResource(R.drawable.search_icon))
+                .position(new LatLng(lat, lng))
+                .snippet("I am here");
+
+        marker = mMap.addMarker(options);
+    }
+
+
+}
